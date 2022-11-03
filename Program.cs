aead42b0be6c96a18d2cafca70f16dee94188524
Tired Maverick <schuetznathaniel@gmail.com>
2022-11-03 14:05:34 -0700

    commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ConverToHex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string str = Console.ReadLine();
+           Console.WriteLine(ConverToHex(str));
+        }
+        public static string ConverToHex(string str)
+        {
+            byte[] hex = System.Text.Encoding.ASCII.GetBytes(str);
+            string hexStr = BitConverter.ToString(hex, 0, hex.Length).Replace("-", " ").ToLower();
+            return hexStr;
+        }
+    }
+}
